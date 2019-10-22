cf7516ed9d4d71b98868166f381e952cd3cef1f4
醉月思 <liqs@dgut.edu.cn>
2019-10-22 12:33:29 +0800

    新增内容安全校验机制

--- pages/train/progress.vue
+++ pages/train/progress.vue
@@ -6,21 +6,21 @@
 					<text class="num" :data-index="index + 1"></text>
 				</view>
 			</view>
-			
+
 			<view class="text-bold text-center margin-top">{{queList[num]}}</view>
 			<view class="cu-form-group margin-top border cu-card article ">
-				<textarea :maxlength="paragraph" @input="textareaAInput" :value="textareaAValue" 
-				:placeholder="'请输入你的想法...(不多于'+ paragraph +'字)'" :show-confirm-bar="false"></textarea>
+				<textarea :maxlength="paragraph" @input="textareaAInput" :value="textareaAValue" :placeholder="'请输入你的想法...(不多于'+ paragraph +'字)'"
+				 :show-confirm-bar="false"></textarea>
 			</view>
 			<view class="flex justify-center bg-white">
 				<button v-if="end" class="cu-btn round bg-gradual-blue margin shadow" @click='initPoster'>完成</button>
 				<button v-else class="cu-btn round bg-gradual-blue margin shadow" @click='nextStep'>下一步</button>
 			</view>
 		</view>
-		
+
 		<view v-show="canvas">
 			<canvas :style="'height: '+canvasHeight+'px;width:'+canvasWidth+'px;'" canvas-id='myCanvas'></canvas>
-			<button  class="cu-btn margin lg block  round bg-gradual-green shadow" @click='saveImage'>保存图片</button>
+			<button class="cu-btn margin lg block  round bg-gradual-green shadow" @click='saveImage'>保存图片</button>
 		</view>
 	</view>
 </template>
@@ -67,46 +67,65 @@
 				this.contents.push(this.textareaAValue)
 				_this.textareaAValue = ''
 				console.log(this.contents)
-				const ctx = uni.createCanvasContext('myCanvas')
-				ctx.drawImage('/static/images/3.png', 0, 0, _this.canvasWidth, _this.canvasHeight)
-				ctx.setFontSize(14)
-				let fontlength = _this.canvasWidth - 60   // 每一行所占空间
-				let textHeight = 15
-				for (let i = 0; i < this.contents.length; i++) {
-					// ctx.fillText(this.contents[i], 30, 40 + i * 40)
-					let allWidth = ctx.measureText(this.contents[i]).width
-					let value = Math.ceil(allWidth/fontlength);   //行数
-					// let textNum = Math.floor(this.contents[i].length/value)
-					let textNum = Math.floor(fontlength/15) +4     //每一行的字数
-					for(let j = 0;j< value;j++) {
-						textHeight = textHeight + 20
-						ctx.fillText(this.contents[i].substring(textNum*j,textNum*(j+1)),15,textHeight)
+
+				wx.cloud.init()
+				wx.cloud.callFunction({
+					name: 'check',
+					data: {
+						"content": this.contents.join()
 					}
-					textHeight = textHeight + 10
-				}
-				ctx.setFontSize(16)
-				ctx.fillText(this.date, _this.canvasWidth-130, _this.canvasHeight-20)
-				ctx.draw(false, () => {
-					setTimeout(() => {
-						uni.canvasToTempFilePath({
-							x: 0,
-							y: 0,
-							width: _this.canvasWidth,
-							height: _this.canvasHeight,
-							canvasId: 'myCanvas',
-							success: function(res) {
-								console.log(res.tempFilePath)
-								_this.imgUrl = res.tempFilePath
-								_this.showModal()
-								// _this.saveImage(_this.imgUrl)
-							},
-							fail: function(res) {
-								console.log(res);
-							}
+				}).then(res => {
+					console.log(res.result)
+					if (res.result.code == 300) {
+						uni.showModal({
+							title: "温馨提示",
+							content: "你所输入的内容可能含有违法违规内容，不支持进行下一步操作"
 						})
-					}, 2000);
+						return
+					} else {
+
+						const ctx = uni.createCanvasContext('myCanvas')
+						ctx.drawImage('/static/images/3.png', 0, 0, _this.canvasWidth, _this.canvasHeight)
+						ctx.setFontSize(14)
+						let fontlength = _this.canvasWidth - 60 // 每一行所占空间
+						let textHeight = 15
+						for (let i = 0; i < this.contents.length; i++) {
+							// ctx.fillText(this.contents[i], 30, 40 + i * 40)
+							let allWidth = ctx.measureText(this.contents[i]).width
+							let value = Math.ceil(allWidth / fontlength); //行数
+							// let textNum = Math.floor(this.contents[i].length/value)
+							let textNum = Math.floor(fontlength / 15) + 4 //每一行的字数
+							for (let j = 0; j < value; j++) {
+								textHeight = textHeight + 20
+								ctx.fillText(this.contents[i].substring(textNum * j, textNum * (j + 1)), 15, textHeight)
+							}
+							textHeight = textHeight + 10
+						}
+						ctx.setFontSize(16)
+						ctx.fillText(this.date, _this.canvasWidth - 130, _this.canvasHeight - 20)
+						ctx.draw(false, () => {
+							setTimeout(() => {
+								uni.canvasToTempFilePath({
+									x: 0,
+									y: 0,
+									width: _this.canvasWidth,
+									height: _this.canvasHeight,
+									canvasId: 'myCanvas',
+									success: function(res) {
+										console.log(res.tempFilePath)
+										_this.imgUrl = res.tempFilePath
+										_this.showModal()
+										// _this.saveImage(_this.imgUrl)
+									},
+									fail: function(res) {
+										console.log(res);
+									}
+								})
+							}, 2000);
+						})
+						this.canvas = true
+					}
 				})
-				this.canvas = true
 			},
 			textareaAInput(e) {
 				this.textareaAValue = e.detail.value
@@ -167,7 +186,6 @@
 					}
 				})
 			}
-
 		},
 		async onLoad(options) {
 			const _this = this
@@ -175,10 +193,10 @@
 			uni.getSystemInfo({
 				success: function(res) {
 					_this.canvasWidth = res.windowWidth
-					_this.canvasHeight = res.windowHeight -10
+					_this.canvasHeight = res.windowHeight - 10
 				}
 			});
-			_this.paragraph = Math.floor(_this.canvasHeight * _this.canvasWidth/(400*_this.queList.length))
+			_this.paragraph = Math.floor(_this.canvasHeight * _this.canvasWidth / (400 * _this.queList.length))
 		}
 	};
 </script>
