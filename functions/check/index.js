4534a22a2ee0902021599058013a345ed683937d
醉月思 <liqs@dgut.edu.cn>
2019-12-10 15:19:36 +0800

    内容安全云调用

--- functions/check/index.js
+++ functions/check/index.js
@@ -6,13 +6,38 @@ cloud.init()
 // 云函数入口函数
 exports.main = async (event, context) => {
 	try {
-		const result = await cloud.openapi.security.msgSecCheck({
+		console.log('待检测文本:' + event.content);
+		let result = await cloud.openapi.security.msgSecCheck({
 			content: event.content
 		})
-		
-		return result
+		console.log('result:' + JSON.stringify(result));
+
+		if (result && result.errCode.toString() === '87014') {
+			return {
+				code: 300,
+				msg: '内容含有违法违规内容',
+				data: result
+			}
+		} else {
+			return {
+				code: 200,
+				msg: 'ok',
+				data: result
+			}
+		}
+
 	} catch (err) {
-		
-		throw err
+		if (err.errCode.toString() === '87014') {
+			return {
+				code: 300,
+				msg: '内容含有违法违规内容',
+				data: err
+			}
+		}
+		return {
+			code: 400,
+			msg: '调用security接口异常',
+			data: err
+		}
 	}
 }
